7bfa3e89439a83e973ad1c1b3b105abc143c6d23
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-21 22:36:32 -0600

    Create onsuccess method if onupgradeneeded handler finished or connected to the database without errors

--- public/db.js
+++ public/db.js
@@ -12,3 +12,18 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true});
 };
+
+// If onupgradeneeded handler which create an object store finished or connected without errors,
+// then trigger onsuccess method.
+request.onsuccess = function(event) {
+
+    // Set the result as the global variable db.
+    db = event.target.result;
+
+    // Check if app is online before reading from db.
+    if (navigator.onLine) {
+
+        // Calling the checkDatabase function.
+        checkDatabase();
+    };
+};
