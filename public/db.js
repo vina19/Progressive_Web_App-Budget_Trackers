d0fd3e421a674b9a0e8045109431b7d5c7ce25ce
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-21 22:21:20 -0600

    Create an object store inside the onupgradeneeded method

--- public/db.js
+++ public/db.js
@@ -2,5 +2,13 @@
 let db;
 
 // Open connection to the indexedDB and name the database as "budget"
-// and set the version to 1
+// and set the version to 1.
 const request = indexedDB.open("budget", 1);
+
+// Create an object store inside the onupgradeneeded method.
+request.onupgradeneeded = function(event) {
+    
+    // An object store called "pending" and set autoIncreament to true.
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true});
+};
