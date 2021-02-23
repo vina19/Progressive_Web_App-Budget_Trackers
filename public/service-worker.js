faea7f010378a9426c1e194bbf9f9015057d520e
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-23 13:35:44 -0600

    Activate the service worker and remove old data from the cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,15 +6,14 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/style.css",
   "/icons/icon-152x152.png",
-  "/icons/icon-192x192.png"
+  "/icons/icon-192x192.png",
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-budget-tracker-v2";
+const DATA_CACHE_NAME = "data-cache-budget-tracker-v1";
 
 // install
 self.addEventListener("install", function (evt) {
-    
   // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
@@ -23,4 +22,22 @@ self.addEventListener("install", function (evt) {
   // tell the browser to activate this service worker immediately once it
   // has finished installing
   self.skipWaiting();
+});
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
