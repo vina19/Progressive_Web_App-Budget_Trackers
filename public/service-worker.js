2ebe3c7e5803cf69f38d85cf2997b6f77139c13c
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-23 13:23:28 -0600

    Add code to install and register service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,26 @@
-console.log("Hi from your service-worker.js file!");
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/manifest.webmanifest",
+  "/style.css",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (evt) {
+    
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});
